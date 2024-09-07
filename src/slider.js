275b47cc507dd7fd9a66c222cd1bad46187b3357
CibNumeritos <imcib2498.212244.2808@gmail.com>
2024-09-06 22:01:27 -0500

    chore: me cago en todo (slider terminado)

--- src/slider.js
+++ src/slider.js
@@ -1,23 +1,28 @@
 let thisImage = 1;
+let swapImageDelay;
 show(thisImage);
 
-function nextImage(control) {
+function nextImage(control, fromButton = false) {
+    if (fromButton) {
+        swapImageDelay = Date.now() + 5000;
+    }
     show(thisImage += control);
 }
 
 function imageSelected(control) {
+    swapImageDelay = Date.now() + 5000;
     show(thisImage = control);
 }
 
 function show(control) {
     let i;
-    
+
     let imageList = document.getElementsByClassName("slider-image");
     let pointList = document.getElementsByClassName("slider-point");
 
     if (control > imageList.length) { thisImage = 1 }
     if (control < 1) { thisImage = imageList.length }
-    
+
     for (i = 0; i < imageList.length; i++) {
         imageList[i].style.display = "none";
     }
@@ -25,7 +30,14 @@ function show(control) {
     for (i = 0; i < pointList.length; i++) {
         pointList[i].className = pointList[i].className.replace(" active", "");
     }
-    
+
     imageList[thisImage - 1].style.display = "block";
     pointList[thisImage - 1].className += " active";
 }
+
+
+setInterval(() => {
+    if (Date.now() > swapImageDelay) {
+        nextImage(1)
+    }
+}, 5000)
